dae9f7102e307119735339e167f4b3b1fda0ee27
Diyar Kudrat <dkudrat@gmail.com>
2020-10-19 11:26:57 -0700

    fixed path issue

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/plaid', plaid);
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+      res.sendFile(path.join('client/build/index.html'));
     });
 }
 
